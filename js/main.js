5063aeff1f1b9a9df381ca8b084ee38101eebe17
Andrew Havr <eclipses.ukr@gmail.com>
2024-06-25 17:24:11 +0300

    Last commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-$(function(){
+"use strict";
 
+
+
+const tabItem = document.querySelectorAll(".tabs__btn-item");
+const tabContent = document.querySelectorAll(".tab__content-item");
+
+tabItem.forEach((e) => {
+   e.addEventListener("click", (evt) => {
+      const tabTarget = evt.currentTarget;
+      const button = tabTarget.dataset.button;
+
+      tabItem.forEach((item) => {
+         item.classList.remove("tabs__btn-item--active");
+      });
+
+      tabContent.forEach((item) => {
+         item.classList.remove("tabs__content-item--active");
+      });
+
+      tabTarget.classList.add("tabs__btn-item--active");
+      document
+         .querySelector(`#${button}`)
+         .classList.add("tabs__content-item--active");
+   });
 });
